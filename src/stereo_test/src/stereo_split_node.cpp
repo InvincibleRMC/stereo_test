a8b9cec0a06e7da7485137a18638f024958a8cd1
Michael Carlstrom <rmc170@case.edu>
2022-09-22 17:34:46 -0700

    camera split

--- src/stereo_test/src/stereo_split_node.cpp
+++ src/stereo_test/src/stereo_split_node.cpp
@@ -57,7 +57,7 @@ image_transport::Subscriber imageSub;
 image_transport::Publisher leftImagePublisher, rightImagePublisher;
 
 // Left and right camera info publishers and messages.
-rclcpp::Publisher<sensor_msgs::msg::CameraInfo> leftCameraInfoPublisher, rightCameraInfoPublisher;
+std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::CameraInfo>> leftCameraInfoPublisher, rightCameraInfoPublisher;
 sensor_msgs::msg::CameraInfo leftCameraInfoMsg, rightCameraInfoMsg;
 
 // Camera info managers.
@@ -66,7 +66,7 @@ camera_info_manager::CameraInfoManager *right_cinfo_;
 
 
 // Image capture callback.
-void imageCallback(const sensor_msgs::msg::Image::ConstPtr& msg)
+void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
 {
     // Get double camera image.
     cv_bridge::CvImagePtr cvImg = cv_bridge::toCvCopy(msg, msg->encoding);
@@ -107,24 +107,24 @@ void imageCallback(const sensor_msgs::msg::Image::ConstPtr& msg)
         cvImage.header.frame_id = msg->header.frame_id;
         cvImage.header.stamp = msg->header.stamp;
         if (leftImagePublisher.getNumSubscribers() > 0
-            || leftCameraInfoPublisher.get_subscription_count() > 0)
+            || (*leftCameraInfoPublisher).get_subscription_count() > 0)
         {
             cvImage.image = use_scaled ? leftScaled : leftImage;
             auto img = cvImage.toImageMsg();
             leftImagePublisher.publish(img);
             leftCameraInfoMsg.header.stamp = img->header.stamp;
             leftCameraInfoMsg.header.frame_id = img->header.frame_id;
-            leftCameraInfoPublisher.publish(leftCameraInfoMsg);
+            (*leftCameraInfoPublisher).publish(leftCameraInfoMsg);
         }
         if (rightImagePublisher.getNumSubscribers() > 0
-            || rightCameraInfoPublisher.get_subscription_count() > 0)
+            || (*rightCameraInfoPublisher).get_subscription_count() > 0)
         {
             cvImage.image = use_scaled ? rightScaled : rightImage;
             auto img = cvImage.toImageMsg();
             rightImagePublisher.publish(img);
             rightCameraInfoMsg.header.stamp = img->header.stamp;
             rightCameraInfoMsg.header.frame_id = img->header.frame_id;
-            rightCameraInfoPublisher.publish(rightCameraInfoMsg);
+            (*rightCameraInfoPublisher).publish(rightCameraInfoMsg);
         }
     }
 }
@@ -133,28 +133,39 @@ void imageCallback(const sensor_msgs::msg::Image::ConstPtr& msg)
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-    auto node_main = rclcpp::Node::make_shared("stereo_splitter_main");
-    auto node_left = node_main.get()->create_sub_node("left").get();
-    auto node_right = node_main.get()->create_sub_node("right").get();
-    //auto node_main = rclcpp::Node("sxs_stereo");
-    //auto node_main_left = rclcpp::Node::image_transport(node_main,"left");
-   // auto node_main_right = rclcpp::Node::image_transport(node_main,"right");
-    //rclcpp::NodeHandle node_main("sxs_stereo");
-    //rclcpp::NodeHandle node_main_left(node_main, "left");
-    //rclcpp::NodeHandle node_main_right(node_main, "right");
+    auto node_main_ = rclcpp::Node::make_shared("stereo_splitter_main");
+    auto node_left_ = node_main_->create_sub_node("left");
+    auto node_right_ = node_main_->create_sub_node("right");
+    
 
-    image_transport::ImageTransport it(node_main);
+    //rclcpp::spin(node_main_);
+
+    //auto node_main_ = rclcpp::Node("sxs_stereo");
+    //auto node_main__left = rclcpp::Node::image_transport(node_main_,"left");
+   // auto node_main__right = rclcpp::Node::image_transport(node_main_,"right");
+    //rclcpp::NodeHandle node_main_("sxs_stereo");
+    //rclcpp::NodeHandle node_main__left(node_main_, "left");
+    //rclcpp::NodeHandle node_main__right(node_main_, "right");
+
+    RCLCPP_INFO(node_main_->get_logger(),"made nodes");
+
+    image_transport::ImageTransport it(node_main_);
+
+    RCLCPP_INFO(node_main_->get_logger(),"image transport");
 
     // Allocate and initialize camera info managers.
-    //new camera_info_manager::CameraInfoManager::CameraInfoManager(node_left,"left camera","");
-    left_cinfo_ = new camera_info_manager::CameraInfoManager(node_left);
-    right_cinfo_ = new  camera_info_manager::CameraInfoManager(node_right);
-    left_cinfo_->loadCameraInfo("");
-    right_cinfo_->loadCameraInfo("");
+    //new camera_info_manager::CameraInfoManager::CameraInfoManager(node_left_,"left camera","");
+    camera_info_manager::CameraInfoManager left_cinfo_(node_left_.get());
+    camera_info_manager::CameraInfoManager right_cinfo_(node_right_.get());
+    RCLCPP_INFO(node_main_->get_logger(),"camera managers");
+    left_cinfo_.loadCameraInfo("");
+    right_cinfo_.loadCameraInfo("");
+
+    RCLCPP_INFO(node_main_->get_logger(),"camera managers");
 
     // Pre-fill camera_info messages.
-    leftCameraInfoMsg = left_cinfo_->getCameraInfo();
-    rightCameraInfoMsg = right_cinfo_->getCameraInfo();
+    leftCameraInfoMsg = left_cinfo_.getCameraInfo();
+    rightCameraInfoMsg = right_cinfo_.getCameraInfo();
 
     // Load node settings.
     std::string inputImageTopic, leftOutputImageTopic, rightOutputImageTopic,
@@ -163,27 +174,31 @@ int main(int argc, char** argv)
     //TODO: add namespace thingy for /stereo
     
     // should use declare_parameters probably
-    inputImageTopic = (*node_main).get_parameter("input_image_topic","input_image_topic_not_set");
-    leftOutputImageTopic = (*node_main).get_parameter("left_output_image_topic","/stereo/left/image_raw");
-    rightOutputImageTopic = (*node_main).get_parameter("right_output_image_topic","/stereo/right/image_raw");
-    leftCameraInfoTopic = (*node_main).get_parameter("left_camera_info_topic","/stereo/left/camera_info");
-    rightCameraInfoTopic = (*node_main).get_parameter("right_camera_info_topic","/stereo/right/camera_info");
-    outputWidth = (*node_main).get_parameter("output_width","0");
-    outputHeight = (*node_main).get_parameter("output_height","0");
+    inputImageTopic = (*node_main_).declare_parameter("input_image_topic","input_image_topic_not_set");
+    leftOutputImageTopic = (*node_main_).declare_parameter("left_output_image_topic","/stereo/left/image_raw");
+    rightOutputImageTopic = (*node_main_).declare_parameter("right_output_image_topic","/stereo/right/image_raw");
+    leftCameraInfoTopic = (*node_main_).declare_parameter("left_camera_info_topic","/stereo/left/camera_info");
+    rightCameraInfoTopic = (*node_main_).declare_parameter("right_camera_info_topic","/stereo/right/camera_info");
+    outputWidth = (*node_main_).declare_parameter("output_width",0);
+    outputHeight = (*node_main_).declare_parameter("output_height",0);
 
-    RCLCPP_INFO(node_main->get_logger(),"input topic to stereo splitter=%s\n", inputImageTopic.c_str());
+    RCLCPP_INFO(node_main_->get_logger(),"declared params");
+
+    RCLCPP_INFO(node_main_->get_logger(),"input topic to stereo splitter=%s\n", inputImageTopic.c_str());
 
     // Register publishers and subscriber.
-    auto leftCameraInfoPublisher = node_left->create_publisher<sensor_msgs::msg::CameraInfo>(leftCameraInfoTopic.c_str(), rclcpp::SensorDataQoS()).get();
-    auto rightCameraInfoPublisher = node_right->create_publisher<sensor_msgs::msg::CameraInfo>(rightCameraInfoTopic.c_str(), rclcpp::SensorDataQoS()).get();
-    imageSub = it.subscribe(inputImageTopic.c_str(), 2, &imageCallback);
+    leftCameraInfoPublisher = node_left_->create_publisher<sensor_msgs::msg::CameraInfo>(leftCameraInfoTopic.c_str(), rclcpp::SensorDataQoS());
+    rightCameraInfoPublisher = node_right_->create_publisher<sensor_msgs::msg::CameraInfo>(rightCameraInfoTopic.c_str(), rclcpp::SensorDataQoS());
+    imageSub = it.subscribe(inputImageTopic.c_str(), 2, imageCallback);
     leftImagePublisher = it.advertise(leftOutputImageTopic.c_str(), 1);
     rightImagePublisher = it.advertise(rightOutputImageTopic.c_str(), 1);
 
+    RCLCPP_INFO(node_main_->get_logger(),"pubs and subs");
+
     // Run node until cancelled.
-    rclcpp::spin_some(node_main);
+    rclcpp::spin(node_main_);
 
     // De-allocate CameraInfoManagers.
-    left_cinfo_ -> ~CameraInfoManager();
-    right_cinfo_ -> ~CameraInfoManager();
+    left_cinfo_.~CameraInfoManager();
+    right_cinfo_.~CameraInfoManager();
 }
